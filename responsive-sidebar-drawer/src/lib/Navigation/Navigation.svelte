c262adb74d047d0202a29d288e43e3f2c1f40685
lethosor <lethosor@gmail.com>
2023-05-12 20:27:05 -0400

    Highlight active page

--- responsive-sidebar-drawer/src/lib/Navigation/Navigation.svelte
+++ responsive-sidebar-drawer/src/lib/Navigation/Navigation.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
 	import { drawerStore } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
 
 	function drawerClose(): void {
 		drawerStore.close();
 	}
+
+	const pages = [
+		{path: '/', name: 'Homepage'},
+		{path: '/about', name: 'About'},
+		{path: '/blog', name: 'Blog'},
+		{path: '/contact', name: 'Contact'},
+	];
+	$: activePath = $page.url.pathname;
 </script>
 
 <nav class="list-nav p-4">
 	<ul>
-		<li><a href="/" on:click={drawerClose}>Homepage</a></li>
-		<li><a href="/about" on:click={drawerClose}>About</a></li>
-		<li><a href="/blog" on:click={drawerClose}>Blog</a></li>
-		<li><a href="/contact" on:click={drawerClose}>Contact</a></li>
+		{#each pages as page}
+			<li key={page.path}><a href={page.path} class={page.path == activePath ? 'bg-primary-active-token' : null} on:click={drawerClose}>{page.name}</a></li>
+		{/each}
 	</ul>
 </nav>
